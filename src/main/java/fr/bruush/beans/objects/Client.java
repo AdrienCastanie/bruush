e0bbd7e581c3029d32fb099b7fa4a75335c98fb2
Anthony Charrier <acharrier@dotscreen.com>
2022-01-06 19:33:19 +0100

    Better qoc and admin section

--- src/main/java/fr/bruush/beans/objects/Client.java
+++ src/main/java/fr/bruush/beans/objects/Client.java
@@ -1,126 +1,132 @@
 package fr.bruush.beans.objects;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.Table;
-
 @Entity
-@Table(name="CLIENT")
+@Table(name = "CLIENT")
 public class Client implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY) 
-	@Column(name="id_client", columnDefinition = "integer")
-	private int id;
-	
-	@Column(name="nom", columnDefinition = "varchar(50)")
-	private String nom;
-	
-	@Column(name="prenom", columnDefinition = "varchar(50)")
-	private String prenom;
-	
-	@Column(name="mail", columnDefinition = "varchar(50)")
-	private String mail;
 
-	@Column(name="mdp", columnDefinition = "varchar(50)")
-	private String mdp;
+    private static final long serialVersionUID = 1L;
 
-	@Column(name="addr", columnDefinition = "varchar(100) default null")
-	private String addr;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_client", columnDefinition = "integer")
+    private int id;
 
-	@Column(name="bloque", columnDefinition = "integer(1) default 0")
-	private int bloque;
-	
-	public Client() {}
+    @Column(name = "nom", columnDefinition = "varchar(50)")
+    private String nom;
 
-	public Client(String nom, String prenom, String mail, String mdp) {
-		this(nom, prenom, mail, null, 0);
-		this.mdp = mdp;
-	}
+    @Column(name = "prenom", columnDefinition = "varchar(50)")
+    private String prenom;
 
-	public Client(String nom, String prenom, String mail, String addr, int bloque) {
-		this.nom = nom;
-		this.prenom = prenom;
-		this.mail = mail;
-		this.addr = addr;
-		this.bloque = bloque;
-	}
+    @Column(name = "mail", columnDefinition = "varchar(50)")
+    private String mail;
 
-	public Client(int id, String nom, String prenom, String mail, String addr, int bloque) {
-		this(nom, prenom, mail, addr, bloque);
-		this.id = id;
-	}
+    @Column(name = "mdp", columnDefinition = "varchar(50)")
+    private String mdp;
 
-	public int getId() {
-		return id;
-	}
+    @Column(name = "addr", columnDefinition = "varchar(100) default null")
+    private String addr;
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    @Column(name = "bloque", columnDefinition = "integer(1) default 0")
+    private int bloque;
 
-	public String getNom() {
-		return nom;
-	}
+    @Column(name = "admin", columnDefinition = "integer(1) default 0")
+    private int admin;
 
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
+    public Client() {
+    }
 
-	public String getPrenom() {
-		return prenom;
-	}
+    public Client(String nom, String prenom, String mail, String mdp) {
+        this(nom, prenom, mail, null, 0);
+        this.mdp = mdp;
+    }
 
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
+    public Client(String nom, String prenom, String mail, String addr, int bloque, int admin) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.mail = mail;
+        this.addr = addr;
+        this.bloque = bloque;
+        this.admin = admin;
+    }
 
-	public String getMail() {
-		return mail;
-	}
+    public Client(String nom, String prenom, String mail, String addr, int bloque) {
+        this(nom, prenom, mail, addr, bloque, 0);
+    }
 
-	public void setMail(String mail) {
-		this.mail = mail;
-	}
+    public Client(int id, String nom, String prenom, String mail, String addr, int bloque) {
+        this(nom, prenom, mail, addr, bloque);
+        this.id = id;
+    }
 
-	public String getAddr() {
-		return addr;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setAddr(String addr) {
-		this.addr = addr;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public int getBloque() {
-		return bloque;
-	}
+    public String getNom() {
+        return nom;
+    }
 
-	public void setBloque(int bloque) {
-		this.bloque = bloque;
-	}
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 
-	public String getMdp() {
-		return mdp;
-	}
+    public String getPrenom() {
+        return prenom;
+    }
 
-	public void setMdp(String mdr) {
-		this.mdp = mdp;
-	}
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public int getBloque() {
+        return bloque;
+    }
+
+    public void setBloque(int bloque) {
+        this.bloque = bloque;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdr) {
+        this.mdp = mdp;
+    }
+
+    public int getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(int admin) {
+        this.admin = admin;
+    }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return "Client{" + "id=" + id + ", nom='" + nom + '\'' + ", prenom='" + prenom + '\'' + ", mail='" + mail + '\'' + ", mdp='" + mdp + '\'' + ", addr='" + addr + '\'' + ", bloque=" + bloque + '}';
     }
 }
--- src/main/webapp/jsp/profile-nav-bar.jsp
+++ src/main/webapp/jsp/profile-nav-bar.jsp
@@ -1,3 +1,5 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <link rel="stylesheet" href="/bruush/css/profile-nav-bar.css">
 <div class="profile-nav-bar-container">
     <a class="decoration-none"
@@ -7,7 +9,7 @@
     <span></span>
     <a id="menu-item-personal-info"
        class="menu-item"
-       href="/bruush/jsp/profile-user-informations.jsp">
+       href="/bruush/action?id=personal-info">
         <h2>Information personnel</h2>
     </a>
     <a id="menu-item-command-history"
@@ -15,14 +17,16 @@
        href="/bruush/action?id=history">
         <h2>Historique de commande</h2>
     </a>
-    <a id="menu-item-admin-users"
-       class="menu-item"
-       href="/bruush/action?id=admin_clients">
-        <h2>Utilisateurs</h2>
-    </a>
-    <a id="menu-item-admin-product"
-       class="menu-item"
-       href="/bruush/action?id=admin_products">
-        <h2>Produits</h2>
-    </a>
+    <c:if test="${ sessionScope.admin == 1 }">
+        <a id="menu-item-admin-users"
+           class="menu-item"
+           href="/bruush/action?id=admin_clients">
+            <h2>Utilisateurs</h2>
+        </a>
+        <a id="menu-item-admin-product"
+           class="menu-item"
+           href="/bruush/action?id=admin_products">
+            <h2>Produits</h2>
+        </a>
+    </c:if>
 </div>
